9056605a8a7e54c3fdd23d44f7d8ac7a19cf726f
serlinecai4935 <scai4935@bths.edu>
2018-12-14 14:31:26 -0500

    board

--- src/Board/gameBoard.java
+++ src/Board/gameBoard.java
@@ -1,5 +1,27 @@
 package Board;
 
+import Rooms.Room;
+
 public class gameBoard {
 
+    private gameBoard [][]map;
+
+    public gameBoard (Room[][] map)
+    {
+        this.map =map;
+    }
+
+    public void print ()
+    {
+        String row = "";
+        for(int i=0; i<map.length; i++)
+        {
+            row = "";
+        }
+        for (int j=0; j<map[i].length; j++)
+        {
+            row += map [i][j].toString();
+        }
+        System.out.println(row);
+    }
 }
